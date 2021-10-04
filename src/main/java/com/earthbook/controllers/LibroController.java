db2b6e209aa1902dc806edbff237ebd2c5fa0923
diegogu <diegogu@gmail.com>
2021-10-04 10:57:09 -0500

    Haciendo el Home

---
+++ src/main/java/com/earthbook/controllers/LibroController.java
@@ -0,0 +1,14 @@
+package com.earthbook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LibroController {
+	@GetMapping({"home", "", "/"})
+	public String home(Model model) {
+		model.addAttribute("titulo", "Home");
+		return "index";
+	}
+}
