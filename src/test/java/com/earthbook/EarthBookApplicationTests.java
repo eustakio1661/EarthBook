54c3dbb84ad49f14d7c1869d0d61acbabe7923a3
diegogu <diegogu@gmail.com>
2021-10-04 10:19:28 -0500

    Iniciando Proyecto

---
+++ src/test/java/com/earthbook/EarthBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earthbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
