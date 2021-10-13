0f26bec1fa6e059292dc9aa53a44c52719a61cbe
djackgongora <dacho4122@gmail.com>
2021-10-13 13:36:14 -0500

    Agregando carrito.html + css + controller

--- src/main/java/com/earthbook/controllers/LibroController.java
+++ src/main/java/com/earthbook/controllers/LibroController.java
@@ -29,5 +29,11 @@ public class LibroController {
 		model.addAttribute("titulo", "Registro");
 		return "registro";
 	}
+	
+	@GetMapping({"carrito", "", "/"})
+	public String carrito(Model model) {
+		model.addAttribute("titulo", "Carrito");
+		return "carrito";
+	}
 
 }
