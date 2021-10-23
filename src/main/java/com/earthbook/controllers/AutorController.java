fa194e5cf733152f2d374c21f9e2d46d74390a08
CesarVega <i201911481@cibertec.edu.pe>
2021-10-23 15:44:06 -0500

    Avances de Mantenimiento 2

--- src/main/java/com/earthbook/controllers/AutorController.java
+++ src/main/java/com/earthbook/controllers/AutorController.java
@@ -1,21 +1,38 @@
 package com.earthbook.controllers;
 
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.earthbook.models.Autor;
+import com.earthbook.repository.IAutorRepository;
 
 @Controller()
 public class AutorController {
-
-
 	
+	private IAutorRepository repoAut;
+
 	@GetMapping("autor/registrar")
 	public String registrar(Model model) {
 	    model.addAttribute("titulo", "Registrar");
 	    return "crudautor";
 	}
+	
+	@PostMapping("autor/grabar")
+	public String grabarPag(@ModelAttribute Autor autor, Model model) {
+		System.out.println("Listo para grabar");
+		System.out.println(autor);
+		repoAut.save(autor);    
+		return "crudautor";
+	}
+	
+	@GetMapping("autor/listado")
+    public String listadoAutor(Model model) {
+        model.addAttribute("titulo", "Listado de libros");
+        return "listadoautores";
+    }
 
 }
 
