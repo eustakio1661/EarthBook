37835fc0638111481243677289142d663bc68fa7
jossrc <kuro.rc10@gmail.com>
2021-10-23 14:40:24 -0500

    Manejando el registro del libro

--- src/main/java/com/earthbook/controllers/LibroController.java
+++ src/main/java/com/earthbook/controllers/LibroController.java
@@ -1,11 +1,33 @@
 package com.earthbook.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.earthbook.models.Libro;
+import com.earthbook.repository.IAutorRepository;
+import com.earthbook.repository.ICategoriaRepository;
+import com.earthbook.repository.IEditorialRepository;
+import com.earthbook.repository.ILibroRepository;
 
 @Controller()
 public class LibroController {
+    
+    @Autowired
+    private ILibroRepository repoLibro;
+    
+    @Autowired
+    private IEditorialRepository repoEditorial;
+    
+    @Autowired
+    private IAutorRepository repoAutor;
+    
+    @Autowired
+    private ICategoriaRepository repoCat;
+    
 	@GetMapping({"home", "", "/"})
 	public String home(Model model) {
 		model.addAttribute("titulo", "Home");
@@ -18,10 +40,33 @@ public class LibroController {
 		return "Categorias";
 	}
 	
-	
 	@GetMapping("libro/registrar")
 	public String registrar(Model model) {
-	    model.addAttribute("titulo", "Registrar");
+
+	    model.addAttribute("titulo", "Registrar Libro");
+	    model.addAttribute("libro", new Libro());
+	    
+	    model.addAttribute("lstCategoria", repoCat.findAll());
+	    model.addAttribute("lstEditoriales", repoEditorial.findAll());
+	    model.addAttribute("lstAutores", repoAutor.findAll());
+	    
+	    model.addAttribute("accionBtn", "Registrar");
+	    return "crudlibro";
+	}
+	
+	@PostMapping("libro/guardar")
+	public String guardar(@ModelAttribute Libro libro, Model model) {
+	    
+	    libro.setSKU(libro.getISBN());
+	    libro.setUrlImagen("FALTA_URL");
+	    
+	    System.out.println(libro);
+	    
+	    model.addAttribute("lstCategoria", repoCat.findAll());
+        model.addAttribute("lstEditoriales", repoEditorial.findAll());
+        model.addAttribute("lstAutores", repoAutor.findAll());
+	    
+	    model.addAttribute("accionBtn", "Registrar");
 	    return "crudlibro";
 	}
 	
