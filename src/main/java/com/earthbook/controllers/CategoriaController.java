c42b9904484f5540939a738cf5be04efdf61d269
CesarVega <i201911481@cibertec.edu.pe>
2021-10-23 14:09:15 -0500

    Avance de Mantenimiento Autor / Categoria

--- src/main/java/com/earthbook/controllers/CategoriaController.java
+++ src/main/java/com/earthbook/controllers/CategoriaController.java
@@ -1,8 +1,16 @@
 package com.earthbook.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller()
 public class CategoriaController {
+	
+	@GetMapping("categoria/registrar")
+	public String registrar(Model model) {
+	    model.addAttribute("titulo", "Registrar");
+	    return "crudcategoria";
+	}
 
 }
