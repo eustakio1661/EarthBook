d028341b7173f4b1f95d8fddf71a2ddf6033d41f
ricardos92 <ricardos_92@hotmail.com>
2021-12-01 08:07:42 -0500

    Contacto controller y tabla

--- src/main/java/com/earthbook/controllers/ContactosController.java
+++ src/main/java/com/earthbook/controllers/ContactosController.java
@@ -1,5 +1,39 @@
 package com.earthbook.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.earthbook.models.Contactos;
+import com.earthbook.repository.IContactosRepository;
+
+@Controller
 public class ContactosController {
+	
+	@Autowired
+	private IContactosRepository repoContacto;
+	
+	@GetMapping("/contactos")
+	public String cargar(Model model) {
+		model.addAttribute("envContacto", new Contactos());
+		return "Contactos";
+	}
+	
+	@PostMapping("/envio")
+	public String grabar(@ModelAttribute Contactos contactos, Model model) {
+		if(contactos == null) {
+			return "contactos";
+		
+		}else {
+			repoContacto.save(contactos); 
+			model.addAttribute("envContacto", new Contactos());
+			model.addAttribute("mensaje", "Se realizo su envio");
+			return "contactos";
+		}
+
+}
 
 }
--- src/main/java/com/earthbook/repository/IContactosRepository.java
+++ src/main/java/com/earthbook/repository/IContactosRepository.java
@@ -1,5 +1,11 @@
 package com.earthbook.repository;
 
-public interface IContactosRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.earthbook.models.Contactos;
+
+@Repository
+public interface IContactosRepository extends JpaRepository<Contactos, Integer> {
+	
 }
